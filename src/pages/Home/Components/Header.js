4fbf914876a27a2d1f3d55073e45d8525121a31a
antony tsygankov <antony.tsygankov@gmail.com>
2021-01-28 21:12:41 -0500

    added header component

---
+++ src/pages/Home/Components/Header.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Settings } from '@material-ui/icons/'
+import {
+    makeStyles,
+    AppBar,
+    Avatar,
+    Toolbar,
+    Typography,
+    IconButton,
+    MenuItem,
+    Menu,
+} from '@material-ui/core/';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    textAlign: 'left',
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (     
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <Avatar alt="wall street bets icon" src="/static/images/logo.png" />
+          </IconButton>
+          <Typography variant="h3" className={classes.title}>
+            Stonks Chat
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <Settings />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+  );
+}
+
+export default Header
