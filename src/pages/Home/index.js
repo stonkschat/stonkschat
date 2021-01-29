84fa6684668696caaefcadc7b71e7e4050a9d539
antony tsygankov <antony.tsygankov@gmail.com>
2021-01-28 21:08:20 -0500

    added header component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Components/Header';
 
 const App = () => {
   return (
     <div className="App">
+        <Header/>
         <header className="App-header">
         Stonks Chat
         </header>
