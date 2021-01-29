75820a41af6c13ecf89c9e36f2ae0a83d48cee22
Ian Lovett <i.c.lovett@gmail.com>
2021-01-29 07:55:08 -0800

    run `prettier` against entire project to autoformat

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,40 +4,40 @@ import { makeStyles, Box, Typography } from '@material-ui/core/';
 import { valueLookup } from 'src/components/Slider/marks';
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 }));
 
 //Temporary function to demonstrate how to use the slider
 const Login = () => {
-    const [sliderValue, setSliderValue] = useState(3); //label value
-    const [filterValue, setFilterValue] = useState(3); // filter value
+  const [sliderValue, setSliderValue] = useState(3); //label value
+  const [filterValue, setFilterValue] = useState(3); // filter value
 
-    //update label here when slider changes values
-    const handleChange = (e, v) => {
-        (sliderValue !== v) && setSliderValue(v);
-    };
+  //update label here when slider changes values
+  const handleChange = (e, v) => {
+    sliderValue !== v && setSliderValue(v);
+  };
 
-    //update filter here when slider is released
-    const commitChange = (e, v) => {
-        (filterValue !== v) &&  setFilterValue(v);
-    };
+  //update filter here when slider is released
+  const commitChange = (e, v) => {
+    filterValue !== v && setFilterValue(v);
+  };
 
-    const classes = useStyles();
-    return (
-        <Box className={classes.container}>
-            <Typography variant='h4' style={{marginBottom: 50}}>
-                {valueLookup[filterValue]}
-            </Typography>
-            <Slider value={sliderValue} handleChange={handleChange} commitChange={commitChange}/>
-        </Box>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Box className={classes.container}>
+      <Typography variant="h4" style={{ marginBottom: 50 }}>
+        {valueLookup[filterValue]}
+      </Typography>
+      <Slider value={sliderValue} handleChange={handleChange} commitChange={commitChange} />
+    </Box>
+  );
+};
 
 export default Login;
