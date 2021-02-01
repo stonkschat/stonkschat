ae69f9d1a49f96f1093c16a878c8f441564faecb
mayja02 <mayja02@gmail.com>
2021-01-31 18:51:15 -0700

    Implements xs UI breakpoint and Tab Interface

--- src/CommentsGridPage.js
+++ src/CommentsGridPage.js
@@ -1,14 +1,74 @@
-import React, { useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
 import { groupBy } from 'lodash';
 import moment from 'moment';
-import { Box } from '@material-ui/core';
+import { Box, useTheme, useMediaQuery, Tabs, Tab, Typography, makeStyles } from '@material-ui/core';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
 import { Comment } from './components/Comment';
 import { SortBySelector, SortEnum } from './components/SortBySelector';
 
+// TODO: separate component (own file)
+//https://material-ui.com/components/tabs/
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+// Tab props
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+    width: '10%',
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
 export const CommentsGridPage = () => {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  //theme and mediaQuery breakpoints
+  const theme = useTheme();
+  const xs = useMediaQuery(theme.breakpoints.down('xs'));
   // fetching comments
   const comments = useQuery(
     'comments',
@@ -79,20 +139,54 @@ export const CommentsGridPage = () => {
       {/* saved in local storage or a cookie */}
       <SortBySelector initialSort={SortEnum.ACCOUNT_AGE} />
 
-      {!!commentGroups && (
-        <Box style={{ display: 'flex', flexGrow: 1, overflow: 'hidden' }} container spacing={3}>
-          {[1, 2, 3, 4].map((groupIndex) => {
-            return (
-              <Box key={groupIndex} style={{ minWidth: '25%', maxHeight: '100vh', overflow: 'auto' }}>
-                {commentGroups[groupIndex]?.map?.((comment) => {
-                  const author = authors.data.find((author) => comment.author === author.name);
-                  return <Comment key={comment.id} comment={comment} author={author} />;
-                })}
-              </Box>
-            );
-          })}
-        </Box>
-      )}
+      {!!commentGroups &&
+        (xs ? (
+          <div className={classes.root}>
+            <Tabs
+              orientation="vertical"
+              variant="scrollable"
+              value={value}
+              onChange={handleChange}
+              aria-label="Vertical tabs example"
+              className={classes.tabs}
+            >
+              {[1, 2, 3, 4].map((groupIndex) => {
+                return (
+                  <Tab label={groupIndex} {...a11yProps(groupIndex)}>
+                    {commentGroups[groupIndex]?.map?.((comment) => {
+                      const author = authors.data.find((author) => comment.author === author.name);
+                      return <Comment key={comment.id} comment={comment} author={author} />;
+                    })}
+                  </Tab>
+                );
+              })}
+            </Tabs>
+            {[1, 2, 3, 4].map((groupIndex) => {
+              return (
+                <TabPanel value={value} index={groupIndex - 1}>
+                  <div>showing panel for group {groupIndex}</div>
+                </TabPanel>
+              );
+              // <Tab label={groupIndex} {...allyProps(groupIndex)}>
+              //   {commentGroups[groupIndex]?.map?.((comment) => {
+              //     const author = authors.data.find((author) => comment.author === author.name);
+              //     return <Comment key={comment.id} comment={comment} author={author} />;
+            })}
+          </div>
+        ) : (
+          <Box style={{ display: 'flex', flexGrow: 1, overflow: 'hidden' }} container spacing={3}>
+            {[1, 2, 3, 4].map((groupIndex) => {
+              return (
+                <Box key={groupIndex} style={{ minWidth: '25%', maxHeight: '100vh', overflow: 'auto' }}>
+                  {commentGroups[groupIndex]?.map?.((comment) => {
+                    const author = authors.data.find((author) => comment.author === author.name);
+                    return <Comment key={comment.id} comment={comment} author={author} />;
+                  })}
+                </Box>
+              );
+            })}
+          </Box>
+        ))}
     </>
   );
 };
